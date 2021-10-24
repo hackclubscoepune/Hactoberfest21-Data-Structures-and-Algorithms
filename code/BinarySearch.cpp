39bcb9e727123458623b35caa6cada5fd62df50f
davidmaged171 <davidmaged171@gmail.com>
2021-10-24 14:00:56 +0200

    Added comments

--- code/BinarySearch.cpp
+++ code/BinarySearch.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+/*
+In this function I devide th array into two parts
+and check the mid of the array.
+There are three possibilities
+
+First: the mid of the array is the target.
+
+Second: the mid of the array is greater than the target
+	in this case we neglect all the half which is greater than the target
+
+Third: the mid of the array is less than the target
+	in this case we neglect all the half which is Less than the target
+
+and every iteration we update the mid of the array.
+*/
 bool BinarySearch(int  arr [],int target ,int n)
 {
     int start=0,end=n-1;
@@ -26,6 +41,7 @@ int main()
     for(int i=0;i<n;i++)
         cin>>arr[i];
 
+//Binary Search algorithm works on Sorted arrays.
     sort(arr,arr+n);
     cout<<"Enter target number: ";
     int target;cin>>target;
